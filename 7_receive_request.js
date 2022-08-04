1f8830a00a9b81a6da1d62f0de090b7da40ff60e
Andrey.Aksenov <Andrey.Aksenov@jetbrains.com>
2022-08-04 11:21:59 +0400

    multipart sample

--- 7_receive_request.js
+++ 7_receive_request.js
@@ -1,22 +1,40 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const multer = require('multer')
 const app = express()
 const port = 3000
 
 app.post('/text', bodyParser.text(), (req, res) => {
     let text = req.body
+    res.send(text)
 })
 
 app.post('/json', bodyParser.json(), (req, res) => {
     let car = req.body
+    res.send(car)
 })
 
-app.post('/urlencoded', bodyParser.urlencoded(), (req, res) => {
-    let car = req.body
+app.post('/urlencoded', bodyParser.urlencoded({extended: true}), (req, res) => {
+    let user = req.body
+    res.send(`The ${user["username"]} account is created`)
 })
 
 app.post('/raw', bodyParser.raw({type: () => true}), (req, res) => {
     let rawBody = req.body
+    res.send(rawBody.toString())
+})
+
+const storage = multer.diskStorage({
+    destination: './uploads/',
+    filename: function (req, file, cb) {
+        cb(null, file.originalname);
+    }
+})
+const upload = multer({storage: storage});
+app.post('/upload', upload.single('image'), function (req, res, next) {
+    let fileDescription = req.body["description"]
+    let fileName = req.file.filename
+    res.send(`${fileDescription} is uploaded to uploads/${fileName}`)
 })
 
 app.listen(port, () => {
