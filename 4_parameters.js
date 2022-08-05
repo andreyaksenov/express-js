15caef826471e3e5fe8f6f74fb4d7faff5b7387a
Andrey.Aksenov <Andrey.Aksenov@jetbrains.com>
2022-08-05 10:05:37 +0400

    query parameters

--- 4_parameters.js
+++ 4_parameters.js
@@ -1,10 +1,17 @@
 const express = require('express')
 const app = express()
+const querystring = require('querystring')
 const port = 3000
 
 app.get('/user/:login', (req, res) => {
     if (req.params['login'] === 'admin') {
         res.send('You are logged in as Admin')
+    }
+})
+
+app.get('/products', (req, res) => {
+    if (req.query['price'] === 'asc') {
+        res.send('Products from the lowest price to the highest')
     }
 })
 
